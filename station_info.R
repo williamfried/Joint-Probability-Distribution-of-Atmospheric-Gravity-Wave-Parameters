3843b20816d2b9ce96be722eb214e5c48060076f
Will Fried <48854149+williamfried@users.noreply.github.com>
2019-04-04 22:31:04 -0400

    Update station_info.R

--- station_info.R
+++ station_info.R
@@ -1,9 +1,9 @@
 # define path to folder where CSV files of the inferred gravity wave parameters are stored
-path_to_data = '/Users/willfried/Desktop/test/Joint-Probability-Distribution-of-Atmospheric-Gravity-Wave-Parameters/'
+path_to_data = ''
 
 # define vector of station names (needs to be in same order as stations are define in station_info.py)
-station_names = c('03020', '23050', '23047', '23023')
+station_names = c()
 
 # specify the CSV file that contains data that will be used to construct the joint probability distribution
 # For Nov-Apr, use 'winter.csv'; for Jun-Sept, use 'summer.csv'; for May and Oct, use 'all.csv'
-dataset = 'summer.csv'
+dataset = ''
