3d17112983ffe42449850f54473364bd68de58ae
Will Fried <48854149+williamfried@users.noreply.github.com>
2019-04-04 22:31:31 -0400

    Update station_info.py

--- station_info.py
+++ station_info.py
@@ -4,16 +4,16 @@ import os
 from math import ceil
 import subprocess
 
-stations = ['03020', '23050', '23047', '23023']
-latitudes = [31.8559, 35.0844, 35.2220, 31.9973]
+stations = []
+latitudes = []
 latitudes = list(map(np.deg2rad, latitudes))
 
 station_latitude = {}
 for i in range(len(stations)):
 	station_latitude[stations[i]] = latitudes[i]
 
-path_to_radiosonde_data = '/Users/willfried/Desktop/radiosonde_data/core_4/'
-path_to_csv_files = '/Users/willfried/Desktop/test/Joint-Probability-Distribution-of-Atmospheric-Gravity-Wave-Parameters/'
+path_to_radiosonde_data = ''
+path_to_csv_files = ''
 
 
 
